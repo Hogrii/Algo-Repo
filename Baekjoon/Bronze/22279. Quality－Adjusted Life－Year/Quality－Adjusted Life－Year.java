e045ac065c7f8c85c5ae84e898826a023603b9ac
JinHo Lee <91480271+Hogrii@users.noreply.github.com>
2026-04-15 09:52:30 +0900

    [Bronze IV] Title: Quality-Adjusted Life-Year, Time: 112 ms, Memory: 14640 KB -BaekjoonHub

---
+++ Baekjoon/Bronze/22279. Quality－Adjusted Life－Year/Quality－Adjusted Life－Year.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        double sum = 0;
+
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            sum += Double.parseDouble(st.nextToken()) * Double.parseDouble(st.nextToken());
+        }
+
+        System.out.printf("%.3f\n", sum);
+    }
+}
